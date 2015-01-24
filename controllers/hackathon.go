db9ae3a2df15a0777c77ed60d99a29c8e4b04c20
Nairitya Khilari <nkmann2@gmail.com>
2015-01-24 18:12:42 +0530

    Add: jhm module and hackathon controller.
    jhm is judge/admin - hackathon mapper.

---
+++ controllers/hackathon.go
@@ -0,0 +1,75 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+// oprations for Hackathon
+type HackathonController struct {
+	beego.Controller
+}
+
+func (c *HackathonController) URLMapping() {
+	c.Mapping("Post", c.Post)
+	c.Mapping("Get", c.Get)
+	c.Mapping("GetAll", c.GetAll)
+	c.Mapping("Put", c.Put)
+	c.Mapping("Delete", c.Delete)
+}
+
+// @Title Post
+// @Description create Hackathon
+// @Param	body		body 	models.Hackathon	true		"body for Hackathon content"
+// @Success 200 {int} models.Hackathon.Id
+// @Failure 403 body is empty
+// @router / [post]
+func (c *HackathonController) Post() {
+
+}
+
+// @Title Get
+// @Description get Hackathon by id
+// @Param	id		path 	string	true		"The key for staticblock"
+// @Success 200 {object} models.Hackathon
+// @Failure 403 :id is empty
+// @router /:id [get]
+func (c *HackathonController) Get() {
+	beego.Info("Yo")
+	c.Redirect("/", 302)
+}
+
+// @Title Get All
+// @Description get Hackathon
+// @Param	query	query	string	false	"Filter. e.g. col1:v1,col2:v2 ..."
+// @Param	fields	query	string	false	"Fields returned. e.g. col1,col2 ..."
+// @Param	sortby	query	string	false	"Sorted-by fields. e.g. col1,col2 ..."
+// @Param	order	query	string	false	"Order corresponding to each sortby field, if single value, apply to all sortby fields. e.g. desc,asc ..."
+// @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
+// @Param	offset	query	string	false	"Start position of result set. Must be an integer"
+// @Success 200 {object} models.Hackathon
+// @Failure 403
+// @router / [get]
+func (c *HackathonController) GetAll() {
+
+}
+
+// @Title Update
+// @Description update the Hackathon
+// @Param	id		path 	string	true		"The id you want to update"
+// @Param	body		body 	models.Hackathon	true		"body for Hackathon content"
+// @Success 200 {object} models.Hackathon
+// @Failure 403 :id is not int
+// @router /:id [put]
+func (c *HackathonController) Put() {
+
+}
+
+// @Title Delete
+// @Description delete the Hackathon
+// @Param	id		path 	string	true		"The id you want to delete"
+// @Success 200 {string} delete success!
+// @Failure 403 id is empty
+// @router /:id [delete]
+func (c *HackathonController) Delete() {
+
+}
